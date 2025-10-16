3f0e288541e852106db5f4899a1855eea624d21b
munawwirch <chmunawwir2005@gmail.com>
2025-10-16 16:03:20 +0530

    nn

--- list.py
+++ list.py
@@ -1,4 +1,11 @@
+<<<<<<< HEAD
 x=[-1,-2,-3,-4,0,1,2,3,4,5]
 for i in x:
     if i > 0:
         print(i)
+=======
+list=[1,-2,36,-33,65]
+for i in list:
+    if(i>0):
+        print (i)
+>>>>>>> 1ad98c5 (nn)
